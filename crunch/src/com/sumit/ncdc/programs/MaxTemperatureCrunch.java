c6493a283456ce5e6ac5fdc90dbfe533260b55f1
sksumit1 <sksumit1@gmail.com>
2015-04-13 15:02:43 +0530

    crunch jars were not detected in map's class path. Added them to the distributed cache

--- crunch/src/com/sumit/ncdc/programs/MaxTemperatureCrunch.java
+++ crunch/src/com/sumit/ncdc/programs/MaxTemperatureCrunch.java
@@ -11,27 +11,22 @@ import org.apache.crunch.fn.Aggregators;
 import org.apache.crunch.impl.mr.MRPipeline;
 import org.apache.crunch.io.To;
 import org.apache.crunch.types.writable.Writables;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
-public class MaxTemperatureCrunch {
+public class MaxTemperatureCrunch extends Configured implements Tool {
   
   public static void main(String[] args) throws Exception {
     if (args.length != 2) {
       System.err.println("Usage: MaxTemperatureCrunch <input path> <output path>");
       System.exit(-1);
     }
-
-    Pipeline pipeline = new MRPipeline(MaxTemperatureCrunch.class);
-    PCollection<String> records = pipeline.readTextFile(args[0]);
-
-    PTable<String, Integer> yearTemperatures = records
-        .parallelDo(toYearTempPairsFn(), Writables.tableOf(Writables.strings(), Writables.ints()));
-    PGroupedTable<String,Integer> groupedTable = yearTemperatures.groupByKey();
-    PTable<String, Integer> maxTemps = groupedTable
-      .combineValues(Aggregators.MAX_INTS());
-    
-    maxTemps.write(To.textFile(args[1]));
-    PipelineResult result = pipeline.done();
-    System.exit(result.succeeded() ? 0 : 1);
+    int res = ToolRunner.run(new Configuration(), new MaxTemperatureCrunch(), args);
+    System.exit(res);   
   }
   static DoFn<String, Pair<String, Integer>> toYearTempPairsFn() {
     return new DoFn<String, Pair<String, Integer>>() {
@@ -45,5 +40,23 @@ public class MaxTemperatureCrunch {
       }
     };
   }
+@Override
+	public int run(String[] args) throws Exception {
+		Configuration config = getConf();
+		//config.addResource("/usr/lib/crunch/crunch-core.jar");
+		DistributedCache.addFileToClassPath(new Path("crunch-core.jar"), config);
+	    Pipeline pipeline = new MRPipeline(MaxTemperatureCrunch.class,config);
+	    PCollection<String> records = pipeline.readTextFile(args[0]);
+	
+	    PTable<String, Integer> yearTemperatures = records
+	        .parallelDo(toYearTempPairsFn(), Writables.tableOf(Writables.strings(), Writables.ints()));
+	    PGroupedTable<String,Integer> groupedTable = yearTemperatures.groupByKey();
+	    PTable<String, Integer> maxTemps = groupedTable
+	      .combineValues(Aggregators.MAX_INTS());
+	    
+	    maxTemps.write(To.textFile(args[1]));
+	    PipelineResult result = pipeline.done();
+	    return result.succeeded() ? 0 : 1;
+	}
 
 }
